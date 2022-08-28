42d633dd52dedf89d86f39f2c37d553715f21b75
Fabio Silva <38165103+Fabioss930@users.noreply.github.com>
2022-08-28 18:39:16 -0400

    Criando pagina de login

---
+++ src/components/input/index.tsx
@@ -0,0 +1,59 @@
+import { useField } from "@unform/core";
+import React, {
+  InputHTMLAttributes,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { IconBaseProps } from "react-icons";
+
+import { Container } from "./styles";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    if (inputRef.current?.value) {
+      setIsFilled(true);
+    } else {
+      setIsFilled(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
---
+++ src/pages/SingIn/style.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+
+  img {
+    margin-top: 30px;
+  }
+`;
+
+export const Content = styled.div`
+  border-radius: 20px;
+
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    margin-top: 20px;
+    color: #ffffff;
+    font-size: 30px;
+    font-family: "Montserrat", Arial, Helvetica, sans-serif;
+    font-weight: bold;
+    margin-bottom: 24px;
+  }
+  form {
+    margin: 50px 0;
+    width: 340px;
+    text-align: center;
+  }
+`;
