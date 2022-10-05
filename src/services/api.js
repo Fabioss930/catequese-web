5e453bb62b51607a19ffd54221bd4de9b6d8112d
Fabio Silva <38165103+Fabioss930@users.noreply.github.com>
2022-10-04 21:26:05 -0400

    Cadastrando turmas

--- src/services/api.js
+++ src/services/api.js
@@ -1,35 +1,67 @@
 import axios from "axios";
 //Funções Relacionadas a integração com backend
 const api = axios.create({
-    baseURL: 'http://localhost:3003/api'
-})
-
-
-const getUsers = async ()=>{
-    try {
-        
-        return await api('/usuario').then((a)=>a.data).catch(error=>console.log('Erro pegar usuarios', error))
-    } catch (error) {
-        console.log('ERRo')
-    }
-    
-}
+  baseURL: "http://localhost:3001/api",
+});
 
 const createUsers = async (data) => {
-    delete data.confirmSenha
-    delete data.turma
-    console.log(data)
+  delete data.confirmSenha;
+  delete data.turma;
+  console.log(data);
 
-    return await api.post('/usuario', data)
-    .then(a => {
-        return {
-            status: 202, message: 'Usuario cadastrado'}
+  return await api
+    .post("/usuario", data)
+    .then((a) => {
+      return {
+        status: 202,
+        message: "Usuario cadastrado",
+      };
+    })
+    .catch((error) => {
+      return {
+        status: 500,
+        message: error.response.data[0].errors,
+      };
+    });
+};
 
-    }).catch(error => {
-        return {
-         status: 500, message: error.response.data[0].errors }
-        })
+const getUsers = async () => {
+  try {
+    return await api("/usuario")
+      .then((a) => a.data)
+      .catch((error) => console.log("Erro pegar usuarios", error));
+  } catch (error) {
+    console.log("ERRO");
+  }
+};
 
-}
+const getClasses = async () => {
+  try {
+    return await api("/turma")
+      .then((a) => a.data)
+      .catch((error) => console.log("Erro", error));
+  } catch (error) {
+    console.log("ERRO");
+  }
+};
 
-export { createUsers, getUsers }
+const classeCatechizing = async (data) => {
+  return await api
+    .post("/turmaCatequista", data)
+    .then((response) => response.data)
+    .then((a) => {
+      return {
+        data,
+        status: 200 || 202,
+        message: "Catequista cadastrado na turma com sucesso",
+      };
+    })
+    .catch((error) => {
+      return {
+        status: 500,
+        message: error.response.data[0].errors,
+      };
+    });
+};
+
+export { createUsers, getUsers, getClasses, classeCatechizing, api };
