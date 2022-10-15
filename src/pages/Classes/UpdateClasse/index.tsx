78860450c4a9d20588d591cf3ad19a9ce334b08f
Fabio Silva <38165103+Fabioss930@users.noreply.github.com>
2022-10-15 10:36:45 -0400

    Renderizando catequistas

--- src/pages/Classes/UpdateClasse/index.tsx
+++ src/pages/Classes/UpdateClasse/index.tsx
@@ -2,12 +2,19 @@ import { Form } from "@unform/web";
 import React, { useState, useCallback, useEffect } from "react";
 import dayjs, { Dayjs } from "dayjs";
 import Header from "../../../components/header/header";
-import { Container, FormSelect, GridBody, ContentButtons } from "./style";
+import {
+  Container,
+  FormSelect,
+  GridBody,
+  ContentButtons,
+  ContainerLoading,
+} from "./style";
 import InputLabel from "@mui/material/InputLabel";
-import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { css, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import Checkbox from "@mui/material/Checkbox";
+
 import ListItemText from "@mui/material/ListItemText";
 import CSS from "csstype";
 import IconButton from "@mui/material/IconButton";
@@ -16,6 +23,8 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import Button from "../../../components/button";
 import { DesktopTimePicker } from "@mui/x-date-pickers/DesktopTimePicker";
 import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 import {
   api,
   classeCatechizing,
@@ -32,6 +41,8 @@ import * as yup from "yup";
 import { date } from "yup/lib/locale";
 import { Co2Sharp, RestartAlt } from "@mui/icons-material";
 import ListItem from "@mui/material/ListItem";
+import _, { filter } from "lodash";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 interface Props {
   id: string;
@@ -65,6 +76,7 @@ interface PropsCatechizing {
 }
 
 const UpdateClasse: React.FC = (props: any) => {
+  const [loading, setLoading] = useState<boolean>(true);
   const [usuariosId, setUsuariosId] = useState<Props[]>([]);
   const [cat, setCat] = useState<Props[]>([]);
   const [classeId, setClasseId] = useState(props.data);
@@ -72,7 +84,7 @@ const UpdateClasse: React.FC = (props: any) => {
     dayjs("2018-01-01T00:00:00.000Z")
   );
   const [personName, setPersonName] = React.useState({ nomes: [], id: [] });
-  const [personNameId, setPersonNameId] = React.useState();
+  const [personNameId, setPersonNameId] = React.useState<Props[]>([]);
   const [names, setNames] = useState<Props[]>([]);
   const [dia_semana, setDia_Semana] = useState("");
   const [hora, setHora] = useState("");
@@ -82,12 +94,6 @@ const UpdateClasse: React.FC = (props: any) => {
     id: null,
   });
 
-  useEffect(() => {
-    handleClasseDate(classeId);
-    getCatechizingClasse();
-    listUsers();
-  }, []);
-
   const handleClasseDate = useCallback(async (id: string) => {
     const classeDate = await getOneClasse(id);
 
@@ -96,16 +102,17 @@ const UpdateClasse: React.FC = (props: any) => {
     setStatus(classeDate.status);
   }, []);
 
-  const getCatechizingClasse = async () => {
+  const listUsers = useCallback(async () => {
+    setLoading(true);
     const response = await getClasseComplete(classeId);
     const nameCat = response.catequistas;
-    setCat(nameCat);
+    const totalCat = nameCat.map((item: Props) => ({
+      id: item.id,
+      nome: item.nome,
+    }));
+    setCat(totalCat);
     console.log("Cat:", cat);
-  };
-
-  const listUsers = useCallback(async () => {
     const users = await getUsers();
-    const idCat = cat.map((item) => item.id);
 
     const usersCat = users.filter(
       (item: PropsClassesUsers) => item.tipo !== "COORDENADOR"
@@ -115,9 +122,18 @@ const UpdateClasse: React.FC = (props: any) => {
       id: item.id,
       nome: item.nome,
     }));
-    const filterCat = totalUser.filter((item: any) => item.id !== idCat);
-    setNames(filterCat);
-    console.log("Nomes:", names);
+
+    const filterNames = _.xorBy(totalUser, totalCat, "id");
+    const nameUsers = filterNames.map((item: any) => ({
+      id: item.id,
+      nome: item.nome,
+    }));
+    setNames(nameUsers);
+    setLoading(false);
+
+    //@ts-ignore
+
+    console.log("PersonName", filterNames);
   }, []);
 
   const ITEM_HEIGHT = 48;
@@ -137,7 +153,7 @@ const UpdateClasse: React.FC = (props: any) => {
     //@ts-ignore
     let vet = [];
     names.forEach((per, id) => {
-      //compara de 'names' e 'value', se der meet ele adiciona o id correspondente
+      //compara de 'names' e 'value', se der meet ele adiciona o id correspodentes
       value.forEach((a) => {
         if (a == per.nome) {
           vet.push(per.id);
@@ -152,6 +168,11 @@ const UpdateClasse: React.FC = (props: any) => {
       id: typeof vet === "string" ? vet.split(",") : vet,
     });
   };
+
+  useEffect(() => {
+    handleClasseDate(classeId);
+    listUsers();
+  }, []);
 
   const handleChangeDay = (event: any) => {
     setDia_Semana(event.target.value);
@@ -241,14 +262,13 @@ const UpdateClasse: React.FC = (props: any) => {
 
       const response = await deleteCatClasse(data);
       if (response.status === 200 || response.status === 202) {
-        alert("Catequista excluído da turma com sucesso");
+        const newNames = cat.filter((item) => item.id !== id);
+        //@ts-ignore
+        setModalConfirm(!modalConfirm);
+        //@ts-ignore
+        setCat(newNames);
+        listUsers();
       }
-
-      const newNames = cat.filter((item) => item.id !== id);
-      //@ts-ignore
-      setModalConfirm(!modalConfirm);
-      //@ts-ignore
-      setUsuariosId(newNames);
     } catch (error) {
       alert("Erro ao excluir catequista da turma");
     }
@@ -271,125 +291,145 @@ const UpdateClasse: React.FC = (props: any) => {
         title="Tem certeza que deseja excluir ? "
       />
       <Header title="Editar turma" />
-      <Container>
-        <GridBody>
-          {/* <FormSelect>
+      {loading ? (
+        <ContainerLoading>
+          <Box sx={{ marginTop: "20%" }}>
+            <CircularProgress />
+          </Box>
+        </ContainerLoading>
+      ) : (
+        <>
+          <Container>
+            <GridBody>
+              {/* <FormSelect>
               <Input name="text" type="text" placeholder="Nome da turma" />
             </FormSelect> */}
-          <FormSelect>
-            <div style={{ display: "flex", width: "100%" }}>
-              <FormControl style={{ width: "100%", marginRight: "8px" }}>
-                <InputLabel id="demo-simple-select-label">
-                  Dia da semana
-                </InputLabel>
-                <Select
-                  id="demo-simple-select"
-                  labelId="demo-simple-select-label"
-                  label="Dia da Semana"
-                  placeholder="Dia da Semana"
-                  onChange={handleChangeDay}
-                  value={dia_semana}
-                >
-                  <MenuItem value="DOMINGO">Domingo</MenuItem>
-                  <MenuItem value="SEGUNDA">Segunda-Feira</MenuItem>
-                  <MenuItem value="TERCA">Terça-Feira</MenuItem>
-                  <MenuItem value="QUARTA">Quarta-Feira</MenuItem>
-                  <MenuItem value="QUINTA">Quinta-Feira</MenuItem>
-                  <MenuItem value="SEXTA">Sexta-Feira</MenuItem>
-                  <MenuItem value="SABADO">Sabádo</MenuItem>
-                </Select>
-              </FormControl>
+              <FormSelect>
+                <div style={{ display: "flex", width: "100%" }}>
+                  <FormControl style={{ width: "100%", marginRight: "8px" }}>
+                    <InputLabel id="demo-simple-select-label">
+                      Dia da semana
+                    </InputLabel>
+                    <Select
+                      id="demo-simple-select"
+                      labelId="demo-simple-select-label"
+                      label="Dia da Semana"
+                      placeholder="Dia da Semana"
+                      onChange={handleChangeDay}
+                      value={dia_semana}
+                    >
+                      <MenuItem value="DOMINGO">Domingo</MenuItem>
+                      <MenuItem value="SEGUNDA">Segunda-Feira</MenuItem>
+                      <MenuItem value="TERCA">Terça-Feira</MenuItem>
+                      <MenuItem value="QUARTA">Quarta-Feira</MenuItem>
+                      <MenuItem value="QUINTA">Quinta-Feira</MenuItem>
+                      <MenuItem value="SEXTA">Sexta-Feira</MenuItem>
+                      <MenuItem value="SABADO">Sabádo</MenuItem>
+                    </Select>
+                  </FormControl>
 
-              <FormControl style={{ width: "100%", marginLeft: "8px" }}>
-                <TextField
-                  InputLabelProps={{ shrink: true }}
-                  type={"time"}
-                  label="Horário"
-                  onChange={handleChangeTime}
-                  value={hora}
-                />
-              </FormControl>
-            </div>
-          </FormSelect>
+                  <FormControl style={{ width: "100%", marginLeft: "8px" }}>
+                    <TextField
+                      InputLabelProps={{ shrink: true }}
+                      type={"time"}
+                      label="Horário"
+                      onChange={handleChangeTime}
+                      value={hora}
+                    />
+                  </FormControl>
+                </div>
+              </FormSelect>
 
-          <FormSelect>
-            <FormControl style={{ width: "100%" }}>
-              <InputLabel id="demo-multiple-checkbox-label">
-                Adicionar catequistas
-              </InputLabel>
-              <Select
-                labelId="demo-multiple-checkbox-label"
-                id="demo-multiple-checkbox"
-                multiple
-                //@ts-ignore
-                value={personName?.nomes}
-                //@ts-ignore
-                onChange={handleChange}
-                input={<OutlinedInput label="Adicionar catequistas" />}
-                //@ts-ignore
-                renderValue={(selected) => {
-                  //@ts-ignore
-                  return selected.join(", ");
-                }}
-                MenuProps={MenuProps}
-              >
-                {names.map((item) => (
+              <FormSelect>
+                <FormControl style={{ width: "100%" }}>
+                  <InputLabel id="demo-multiple-checkbox-label">
+                    Adicionar catequistas
+                  </InputLabel>
+                  <Select
+                    labelId="demo-multiple-checkbox-label"
+                    id="demo-multiple-checkbox"
+                    multiple
+                    //@ts-ignore
+                    value={personName?.nomes}
+                    //@ts-ignore
+                    onChange={handleChange}
+                    input={<OutlinedInput label="Adicionar catequistas" />}
+                    //@ts-ignore
+                    renderValue={(selected) => {
+                      //@ts-ignore
+                      return selected.join(", ");
+                    }}
+                    MenuProps={MenuProps}
+                  >
+                    {names.map((item) => (
+                      //@ts-ignore
+                      <MenuItem key={item.id} value={item.nome}>
+                        <Checkbox
+                          //@ts-ignore
+                          checked={personName?.nomes.includes(item.nome)}
+                          name={item.nome}
+                        />
+                        <ListItemText primary={item.nome} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </FormSelect>
+              <FormSelect>
+                <FormControl style={{ width: "100%" }}>
+                  <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                  <Select
+                    id="demo-simple-select"
+                    label="Status"
+                    placeholder="Status"
+                    value={status}
+                    onChange={handleChangeStatus}
+                  >
+                    <MenuItem value="ATIVO">Ativo</MenuItem>
+                    <MenuItem value="INATIVO">Inativo</MenuItem>
+                  </Select>
+                </FormControl>
+              </FormSelect>
+              <InputLabel>Catequistas da turma</InputLabel>
+
+              <FormControl>
+                {cat.map((item) => (
                   //@ts-ignore
                   <MenuItem key={item.id} value={item.nome}>
-                    <Checkbox
-                      //@ts-ignore
-                      checked={personName?.nomes.includes(item.nome)}
-                      name={item.nome}
+                    <ListItemText
+                      primary={item.nome}
+                      style={{ color: "black" }}
                     />
-                    <ListItemText primary={item.nome} />
+
+                    <IconButton
+                      onClick={() => handleModalConfirm(item.id)}
+                      edge="end"
+                      aria-label="delete"
+                    >
+                      <DeleteIcon />
+                    </IconButton>
                   </MenuItem>
                 ))}
-              </Select>
-            </FormControl>
-          </FormSelect>
-          <FormSelect>
-            <FormControl style={{ width: "100%" }}>
-              <InputLabel id="demo-simple-select-label">Status</InputLabel>
-              <Select
-                id="demo-simple-select"
-                label="Status"
-                placeholder="Status"
-                value={status}
-                onChange={handleChangeStatus}
-              >
-                <MenuItem value="ATIVO">Ativo</MenuItem>
-                <MenuItem value="INATIVO">Inativo</MenuItem>
-              </Select>
-            </FormControl>
-          </FormSelect>
-          <InputLabel>Catequistas da turma</InputLabel>
-
-          <FormControl>
-            {cat.map((item) => (
-              //@ts-ignore
-              <MenuItem key={item.id} value={item.nome}>
-                <ListItemText primary={item.nome} style={{ color: "black" }} />
-
-                <IconButton
-                  onClick={() => handleModalConfirm(item.id)}
-                  edge="end"
-                  aria-label="delete"
+              </FormControl>
+              <ContentButtons>
+                <Button
+                  type="button"
+                  onClick={handleSubmitForm}
+                  style={Cadastrar}
                 >
-                  <DeleteIcon />
-                </IconButton>
-              </MenuItem>
-            ))}
-          </FormControl>
-          <ContentButtons>
-            <Button type="button" onClick={handleSubmitForm} style={Cadastrar}>
-              Salvar
-            </Button>
-            <Button style={Cancelar} onClick={(event) => props.navTo(event, 5)}>
-              Cancelar
-            </Button>
-          </ContentButtons>
-        </GridBody>
-      </Container>
+                  Salvar
+                </Button>
+                <Button
+                  style={Cancelar}
+                  onClick={(event) => props.navTo(event, 5)}
+                >
+                  Cancelar
+                </Button>
+              </ContentButtons>
+            </GridBody>
+          </Container>
+        </>
+      )}
     </>
   );
 };
